221e47859d05ee83a60fb36363493b199b638873
Lucas Livchits <livchitsl@gmail.com>
2020-02-01 20:16:34 -0300

    Change BASE_URL

--- api.js
+++ api.js
@@ -3,14 +3,14 @@ const got = require('got');
 
 dotenv.config();
 
-const BASE_URL = 'https://api.themoviedb.org/3/movie/top_rated?';
+const BASE_URL = 'https://api.themoviedb.org/3/movie/top_rated?api_key=';
 const { API_KEY } = process.env;
 
 const movies = [];
 
 async function getMovies(page) {
   try {
-    const { results } = await got(`${BASE_URL}api_key=${API_KEY}&language=en-US&page=${page}`);
+    const { results } = await got(`${BASE_URL}${API_KEY}&language=en-US&page=${page}`);
   } catch (error) {
     console.log(error.response.body);
   }
