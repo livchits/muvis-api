f3127e91812b527f99836e68ba20004d0568a474
Lucas Livchits <livchitsl@gmail.com>
2020-02-07 12:14:38 -0300

    Add function to validate date format and range

--- ajv.js
+++ ajv.js
@@ -1,5 +1,7 @@
 const Ajv = require('ajv');
 const ajv = new Ajv({ useDefaults: 'empty', removeAdditional: true });
+const isValid = require('date-fns/isValid');
+const isWithinInterval = require('date-fns/isWithinInterval');
 
 const schema = {
   additionalProperties: false,
@@ -25,3 +27,14 @@ const schema = {
     rate: { type: 'number' }
   }
 };
+
+function isValidDate(date) {
+  const dateObject = new Date(date);
+  return (
+    isValid(dateObject) &&
+    isWithinInterval(dateObject, {
+      start: new Date(1880, 0, 1),
+      end: new Date()
+    })
+  );
+}
