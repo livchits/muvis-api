41a2d3ebc1988ef20cb483644977085eab976319
Lucas Livchits <livchitsl@gmail.com>
2020-02-07 11:44:32 -0300

    Add function add a movie to the db

--- db.js
+++ db.js
@@ -1,11 +1,21 @@
-const low = require("lowdb");
-const FileSync = require("lowdb/adapters/FileSync");
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
 
 function createDb() {
-  const adapter = new FileSync("movies.json");
+  const adapter = new FileSync('movies.json');
   const db = low(adapter);
 
   db.defaults({ movies: [] }).write();
 
   return db;
+}
+
+function addMovie(db, movie) {
+  if (validate(movie)) {
+    db.get('movies')
+      .push(movie)
+      .write();
+  } else {
+    console.error(validate.errors);
+  }
 }
