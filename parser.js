70f103d245f524dbeb1f54ea8bebe088ca576ca8
Lucas Livchits <livchitsl@gmail.com>
2020-02-08 00:05:15 -0300

    Add function to change genres id for its names

--- parser.js
+++ parser.js
@@ -32,3 +32,22 @@ function formatDate(dateString) {
   const formatedDate = new Intl.DateTimeFormat('en-US', options).format(date);
   return formatedDate;
 }
+
+function changeGenresIDsforNames(moviesList, genresIdsAndNamesList) {
+  //itera la lista de películas
+  moviesList.forEach(movie => {
+    //de cada película mapea el array genre_ids
+    const genresNames = movie.genre_ids.map(id => {
+      //se itera la lista de objetos {id: género}
+      genresIdsAndNamesList.forEach(genre => {
+        //si el id es igual al del listado de géneros, le asigna el nombre como valor
+        if (genre.id === id) {
+          id = genre.name;
+        }
+      });
+      return id; //se devuelve el id
+    });
+    movie.genre_ids = genresNames; //se asigna a la propiedad genres_ids de la película el nuevo array
+  });
+  return moviesList;
+}
