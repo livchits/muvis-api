f02fb40c0d899c6ee922618283ed8ebb809c58be
Lucas Livchits <livchitsl@gmail.com>
2020-02-08 00:26:10 -0300

    Add router for GET to /:id

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', (req, res) => {
   res.json(getMovies());
 });
 
+router.route('/:id').get((req, res, next) => {
+  const { id } = req.params;
+  const movie = findMovieById(id);
+  if (movie) {
+    res.json(movie);
+  } else {
+    const err = new Error(`404 - The movie with the id ${id} was not found`);
+    err.status = 404;
+    next(err);
+  }
+});
+
 module.exports = router;
