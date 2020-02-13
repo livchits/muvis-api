ffc9e06cb2f1db2218496262636c2ec013e8c74e
Lucas Livchits <livchitsl@gmail.com>
2020-02-13 12:37:45 -0300

    Now findMovieById return a lodash wrapper

--- movies.js
+++ movies.js
@@ -2,9 +2,7 @@ const { getMovies } = require('./db');
 
 function findMovieById(id) {
   const moviesList = getMovies();
-  //const movieWithId = moviesList.find({ id: Number(id) }).value(); //trabaja con el lodash wrapper y devuelve un objeto
-  const movieWithId = moviesList.find(movie => movie.id === Number(id)); // trabaja con el array
-
+  const movieWithId = moviesList.find({ id: Number(id) });
   return movieWithId;
 }
 
