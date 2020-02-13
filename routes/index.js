7b72f21d3a3cddb1b9b6f036e505cd88a76f431c
Lucas Livchits <livchitsl@gmail.com>
2020-02-13 12:38:43 -0300

    Add comments

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,14 @@ const router = express.Router();
 const { getMovies } = require('../db');
 
 router.get('/', (req, res) => {
-  res.json(getMovies());
+  res.json(getMovies()); //chequear que efectivamente la api devuelva las películas o agregar un .value()
 });
 
 router.route('/:id').get((req, res, next) => {
   const { id } = req.params;
   const movie = findMovieById(id);
   if (movie) {
-    res.json(movie);
+    res.json(movie); //chequear que efectivamente la api devuelva al película o modificar findMovieById
   } else {
     const err = new Error(`404 - The movie with the id ${id} was not found`);
     err.status = 404;
