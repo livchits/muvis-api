4c12aa92f5a2cbe55b4891ae24e095ac26cd458e
Lucas Livchits <livchitsl@gmail.com>
2020-05-02 19:21:13 -0300

    Add validation for puts

--- validations.js
+++ validations.js
@@ -2,6 +2,28 @@ const { isValidDate } = require('./ajv');
 const { check, validationResult } = require('express-validator');
 const { capitalize, formatDate } = require('./utils');
 
+const genres = [
+  'action',
+  'adventure',
+  'animation',
+  'comedy',
+  'crime',
+  'documentary',
+  'drama',
+  'family',
+  'fantasy',
+  'history',
+  'horror',
+  'music',
+  'mistery',
+  'romance',
+  'science fiction',
+  'tv movie',
+  'thriller',
+  'war',
+  'western',
+];
+
 const postValidation = [
   check('title')
     .isLength({ min: 2 })
@@ -29,29 +51,44 @@ const postValidation = [
     .custom(
       (data) =>
         Array.isArray(data) &&
-        data.every((genre) =>
-          [
-            'action',
-            'adventure',
-            'animation',
-            'comedy',
-            'crime',
-            'documentary',
-            'drama',
-            'family',
-            'fantasy',
-            'history',
-            'horror',
-            'music',
-            'mistery',
-            'romance',
-            'science fiction',
-            'tv movie',
-            'thriller',
-            'war',
-            'western',
-          ].includes(genre.toLowerCase())
-        )
+        data.every((genre) => genres.includes(genre.toLowerCase()))
+    )
+    .customSanitizer((value) => value.map((genre) => capitalize(genre)))
+    .withMessage('Movie genre is required.'),
+];
+
+const putValidation = [
+  check('title')
+    .optional()
+    .isLength({ min: 2 })
+    .withMessage('Name must have more than 5 characters.'),
+  check('date')
+    .optional()
+    .custom((value) => {
+      if (!isValidDate(value))
+        throw new Error(
+          'Movie date is required and should be in yyyy-mm-dd format.'
+        );
+      return true;
+    }),
+  check('overview')
+    .optional()
+    .isLength({ min: 50 })
+    .withMessage(
+      'Movie description is required and should have minimum 50 characters.'
+    ),
+  check('rate')
+    .optional()
+    .isNumeric({ min: 1, max: 10 })
+    .withMessage(
+      'Movie rate is required and should be a number between 1 and 10.'
+    ),
+  check('genres')
+    .optional()
+    .custom(
+      (data) =>
+        Array.isArray(data) &&
+        data.every((genre) => genres.includes(genre.toLowerCase()))
     )
     .customSanitizer((value) => value.map((genre) => capitalize(genre)))
     .withMessage('Movie genre is required.'),
