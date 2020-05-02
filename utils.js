e8764ff32f01d3be456e81e8dd8b00e7d71aed11
Lucas Livchits <livchitsl@gmail.com>
2020-05-02 19:27:47 -0300

    Add capitalize function

--- utils.js
+++ utils.js
@@ -4,3 +4,12 @@ function formatDate(dateString) {
   const formatedDate = new Intl.DateTimeFormat('en-US', options).format(date);
   return formatedDate;
 }
+
+function capitalize(word) {
+  const letters = word.toLowerCase().split('');
+  letters[0] = letters[0].toUpperCase();
+  const capitalizeWord = letters.join('');
+  return capitalizeWord;
+}
+
+module.exports = { capitalize, formatDate };
